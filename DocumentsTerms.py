bb608ee0590035bbc31b0eb5726b0dc2d22a3842
carlos <kalelcarlosmartinez@gmail.com>
2021-05-05 13:52:01 -0700

    ya obtengo el vector de pesos por cada documento falta redondear los calculos a 2 lugares despues de la coma y revisar para que sea mas rapido que esta un poco lento para mi gusto

--- DocumentsTerms.py
+++ DocumentsTerms.py
@@ -1,3 +1,5 @@
+import math
+
 def turnLineToWords(line):
     wordList = {}
     word = ''
@@ -32,12 +34,12 @@ def documentId(d):
         return False
     return True
 
-
 def readDocument(url):
     doc = open(url,"r")
     lines = doc.readlines()
     doc.close()
     documentWordList = {}
+    documentsWords = {}
     documentList = []
     for item in lines:
         temp = turnLineToWords(item)
@@ -45,22 +47,68 @@ def readDocument(url):
             documentWordList = sumDict(documentWordList,temp)
         elif temp.get('.I') != None and int(list(temp)[1]) > 1:
             documentList.append((int(list(temp)[1]) - 1,documentWordList))
+            documentsWords = sumDict(documentsWords,documentWordList)
             documentWordList = {}
             #Estas lineas comentadas limitan la cantidad de documentos analizados
             #if int(list(temp)[1]) > 2:
             #    break
     if documentWordList != None:
         documentList.append((len(documentList) + 1,documentWordList))
-    return documentList
+        documentsWords = sumDict(documentsWords,documentWordList)
+    return documentsWords , documentList
+
+def weightVector_tf(document,wordList):
+    weightVector = {}
+    max_freq = -1
+    value = 0
+    for item in wordList:
+        if document.get(item) != None:
+            weightVector[item] = document[item]
+            if document[item] > max_freq:
+                max_freq = document[item]
+        else:
+            weightVector[item] = 0
+    for item in weightVector:
+        weightVector[item] = weightVector[item]/max_freq
+    return weightVector
+
+def matrix_tf(documentList,documentsWords):
+    matrix = []
+    for item in documentList:
+        row = weightVector_tf(item[1],documentsWords)
+        matrix.append(list(row.items()))
+    return matrix
+
+def amount_doc_appear_term_i(documentList,termino):
+    result = 0
+    for item in documentList:
+        if item[1].get(termino):
+            result += 1
+    return result
+
+def weightVector_idf(documentList,documentsWords):
+    weightVector = {}
+    amountDocCollection = len(documentList)
+    for item in documentsWords:
+        weightVector[item] = math.log10(amountDocCollection/amount_doc_appear_term_i(documentList,item))
+    return weightVector
+
+def matrix_w_ij(documentList,documentsWords):
+    matrix_w = []
+    tf = matrix_tf(documentList,documentsWords)
+    idf = weightVector_idf(documentList, documentsWords)
+    for i in range(len(tf)):
+        matrix_w.append([])
+        for j in range(len(idf)):
+            term = tf[i][j][0]
+            matrix_w[i].append((term,tf[i][j][1]*idf[term]))
+    return matrix_w
 
 def main():
     url = 'collections/cran.txt'
-    result = readDocument(url)
-    
-    for i in result:
-        print(i[0])
-        print(i[1])
-        print()
-    print(len(result))
+    documentsWords ,documentList = readDocument(url)
+    r = matrix_w_ij(documentList,documentsWords)
+    print(r)
+
 if __name__ == '__main__':
     main()
